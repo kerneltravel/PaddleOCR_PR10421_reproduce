7ccfed344e88e371ee744a335e7dd4b3fff0ffca
kerneltravel <kjpioo2006@gmail.com>
2023-07-30 22:47:46 +0800

    use --collect-all paddleocr param to pyinstaller to fix the root cause

--- to_exe.py
+++ to_exe.py
@@ -6,4 +6,4 @@ dirPath = os.getcwd()
 
 os.system(f'cd /d {dirPath}')
 
-os.system(r'pyinstaller.exe -F --add-data .\paddleocr;.\paddleocr    --add-data .\mklml.dll;. main.py')
+os.system(r'pyinstaller.exe -F --collect-all paddleocr --add-data .\paddleocr;.\paddleocr    --add-data .\mklml.dll;. main.py')
